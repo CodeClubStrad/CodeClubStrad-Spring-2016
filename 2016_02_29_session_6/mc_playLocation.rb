52c5b1f2998e1cf27d00fe4b76e2caaae3c0da20
Ben Anderson <dataknut@icloud.com>
2016-02-29 13:39:39 +0000

    move mc_message out of loop

--- 2016_02_29_session_6/mc_playLocation.rb
+++ 2016_02_29_session_6/mc_playLocation.rb
@@ -1,15 +1,19 @@
 # mc_playLocation
+# (c) 2016 @dataknut CC-BY-NC
 # A simple SonicPi live loop to play a note that represents
 # our Minecraft Pi location (x,y,z) values
 # uses pan (left vs right speaker) for x - see 2.2 Synth Options
 # uses the note for y (height)
 # uses amp (amplitude) for z - see 2.2 Synth Options
 
-# @dataknut
+# Set this loop running and then move around in MinecraftPi
+
+# post a message to Minecraft to prove the connection is working
+mc_message "Hello from Sonic Pi"
+
 live_loop :mc_playLocation do
   # post a message to Minecraft to prove the connection is working
-  mc_message "Hello from Sonic Pi"
-  
+ 
   # Turn x into a useful pan value
   # Pan has the range -1 (left) to 1 (right)
   # Use the fact that the MinecraftPi world goes from -128 to 128 on any dimension
@@ -23,7 +27,13 @@ live_loop :mc_playLocation do
   # See how we have to use .abs to always give a positive value
   zAmp = (mc_location[2].abs/128)*2
 
-  puts xPan yNote zAmp
+  # print location
+  puts mc_location
+  # print the converted values
+  puts "xPan =", xPan, "yNote =", xPan, "zAmp =", zAmp
+
+  # Now play the note
   play yNote, pan: xPan, amp: zAmp, release: 0.5
+  # Wait 1/2 a second
   sleep 0.5
 end
